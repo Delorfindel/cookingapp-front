c5fa5e1f7f6196d9e10ff223022465c3e5cd9315
Kirsten <kerirath.hor@gmail.com>
2020-05-27 02:02:28 +0200

    update: feed page

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -6,24 +6,6 @@ import ActionsHeader from './ActionsHeader';
 import RecipesList from './RecipesList';
 
 export default function Feed() {
-  const [FeedState, dispatch] = useFeedContext();
-  const { recipes } = FeedState;
-  // const addLol = () => {
-  //   dispatch({
-  //     type: 'addRecipes',
-  //     payload: [
-  //       {
-  //         id: 123456544,
-  //         name: 'mabite',
-  //       },
-  //       {
-  //         id: 123456544666,
-  //         name: 'mabiteDur',
-  //       },
-  //     ],
-  //   });
-  // };
-
   return (
     <>
       <ActionsHeader />
