d9bf0c2b2e0286bbbe11e71b6dbb97c15cca7455
Delorfindel <gantier.dany@gmail.com>
2020-05-30 02:46:02 +0200

    fix: try 7

--- src/lib/withApollo.tsx
+++ src/lib/withApollo.tsx
@@ -1,11 +1,11 @@
-import withApollo from "next-with-apollo"
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import withApollo from 'next-with-apollo';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 
 export default withApollo(
-    ({ initialState, ctx }) =>
-        new ApolloClient({
-            uri: 'https://cyberpatrimoine-backend.herokuapp.com/graphql',
-            cache: new InMemoryCache().restore(initialState || {}),
-            ssrMode: Boolean(ctx),
-        })
-)
+  ({ initialState, ctx }) => new ApolloClient<NormalizedCacheObject>({
+    uri: 'https://cyberpatrimoine-backend.herokuapp.com/graphql',
+    cache: new InMemoryCache().restore(initialState || {}),
+    ssrMode: Boolean(ctx),
+  }),
+);
