6e2ed5a80ad14531a4623db337c9d51431e07c1e
Kirsten <kerirath.hor@gmail.com>
2020-05-31 01:18:29 +0200

    add: register page with error handling

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -13,7 +13,6 @@ export default function LoginForm() {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [confirmPasswordError, setConfirmPasswordError] = useState(false);
   const [registerError, setRegisterError] = useState(false);
-  const [registerErrorMsg, setRegisterErrorMsg] = useState('');
   const [AuthState, dispatch] = useAuthContext();
 
   const handleSubmit = (e) => {
@@ -46,12 +45,19 @@ export default function LoginForm() {
             </div>
             <div className="">
               <input
-                className="w-full px-4 py-2 text-gray-700 bg-white shadow-lg appearance-none rounded-xl"
+                className={`w-full px-4 py-2 text-gray-700 bg-white shadow-lg appearance-none rounded-xl ${registerError && 'border-red-400'}`}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
+            {registerError && (
+              <div className="flex flex-1 mt-2">
+                <a className="text-sm text-red-500 mt-2" href="#">
+                  Cet email est déjà utilisé.
+              </a>
+              </div>
+            )}
           </div>
           <div className="items-center mb-6">
             <div className="">
